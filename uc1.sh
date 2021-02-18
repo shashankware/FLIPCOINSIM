6743f8351b93ddf47fb689bb4de382d33ed57d09
shashank <shashank.w1998@gmail.com>
2021-02-18 17:58:46 +0530

    no. of times h&t

--- uc1.sh
+++ uc1.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#!/bin/bash
 random=$((RANDOM%2))
 
 if [ $random -eq 0 ]
@@ -8,3 +7,19 @@ then
 else
 	echo "Tail is winner"
 fi
+
+
+#!/bin/bash
+echo  "Enter number of time coin flip"
+read n
+for((i=0;i<n;i++))
+do
+random=$((RANDOM%2))
+	if [ $random -eq 0 ]
+	then
+        	echo "Head is winner"
+	else
+        	echo "Tail is winner"
+	fi
+done
+
