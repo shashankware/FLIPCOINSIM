5d21e910e6e3a974031643bd12a52ae97980494e
shashank <shashank.w1998@gmail.com>
2021-02-18 18:06:50 +0530

    win or tie

--- uc1.sh
+++ uc1.sh
@@ -23,3 +23,21 @@ random=$((RANDOM%2))
 	fi
 done
 
+
+#!/bin/bash
+noOfHead=0
+noOfTail=0
+for((i=1;i<21;i++))
+do
+	coin=$((RANDOM%2))
+	if [ $coin -eq 0 ]
+	then
+((noOfHead=noOfHead+1))
+	else
+((noOfTail=noOfTail+1))
+	fi
+done
+        echo "head count is" $noOfHead
+        echo "tail count is" $noOfTail
+
+
